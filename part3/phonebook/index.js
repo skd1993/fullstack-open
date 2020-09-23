2b4dac0fed9b01a14c4b39169902318beab3f342
skd1993 <shobhit.kr@outlook.com>
2020-09-23 15:42:22 +0530

    part 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+
+morgan.token('host', function (req, res) {
+  return req.hostname;
+});
+
+app.use(morgan('tiny'));
 
 let persons = [
   {
