a66cd1e216a139f7f1a314b6d5e257b1f0b2ffb5
skd1993 <shobhit.kr@outlook.com>
2020-09-23 15:28:18 +0530

    part 3.1-3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: 'Shobhit',
+    number: '12132313',
+    id: 1,
+  },
+  {
+    name: 'Brij',
+    number: '1212',
+    id: 2,
+  },
+  {
+    name: 'Shrey',
+    number: '12321',
+    id: 3,
+  },
+  {
+    name: 'Aditya',
+    number: '12312',
+    id: 4,
+  },
+  {
+    name: 'Arun',
+    number: '232',
+    id: 4,
+  },
+  {
+    name: 'Himanshu',
+    number: '31231',
+    id: 5,
+  },
+  {
+    name: 'Pranav',
+    number: '12123',
+    id: 6,
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  let len = persons.length;
+  let dt = new Date();
+  res.send(`<p>Phonebook has info for ${len} people</p><p>${dt}</p>`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  let id = Number(req.params.id);
+  const result = persons.find((person) => id === person.id);
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(400).send('Nothing found, error 400').end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  let id = Number(req.params.id);
+  const result = persons.filter((person) => id !== person.id);
+  persons = result;
+  res.status(204).send(`Deleted id ${id}`).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  let body = req.body;
+  console.log(req);
+  if (!body.name || !body.number) {
+    res.status(400).send('name or number cannot be empty').end();
+  }
+  const duplicateName = persons
+    .map((person) => person.name)
+    .includes(body.name);
+  if (duplicateName) {
+    res.status(400).send({ error: 'name must be unique' }).end();
+  } else {
+    let new_id = Math.floor(Math.random() * 1000);
+    body.id = new_id;
+    persons = persons.concat(body);
+    res.json(persons);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
