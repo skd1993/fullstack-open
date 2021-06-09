76032a142453c45ee42a3d02f526d559e52e17ec
shobhit_kumar <shobhit.kr@outlook.com>
2021-06-09 15:58:47 +0530

    4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch(() => {
+    logger.error('error connecting to MongoDB', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
