909989b72e9b743549f9ac95cbe66a96b8c8a724
shobhit_kumar <shobhit.kr@outlook.com>
2021-06-29 17:53:11 +0530

    5.11-5.12

--- part5/src/components/Togglable.js
+++ part5/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React, {useState, useImperativeHandle} from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = React.forwardRef((props, ref) => {
 	const [visibility, setVisibility] = useState(false);
@@ -21,5 +22,14 @@ const Togglable = React.forwardRef((props, ref) => {
 		</div>
 	)
 })
+
+Togglable.propTypes = {
+	buttonName
+:
+PropTypes.string.isRequired,
+	cancelButtonName
+:
+PropTypes.string.isRequired
+}
 
 export default Togglable
