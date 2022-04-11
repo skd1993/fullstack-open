4fae39e042c93ebfa7b74384ff55391a1761e9cf
skd1993 <shobhit.kr@outlook.com>
2022-04-11 11:21:39 +0530

    8.13-8.16

---
+++ part8/server/schema.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2,
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author',
+  },
+  genres: [{ type: String }],
+});
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+  },
+  born: {
+    type: Number,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 6,
+  },
+  token: {
+    type: String,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  favoriteGenre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+const Author = mongoose.model('Author', authorSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = { Book, Author, User };
